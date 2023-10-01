42b62564b27f8ca9d60fe32b673f8dd0942de46b
doantaa <doantaaloycius@gmail.com>
2023-10-01 22:42:51 +0700

    Create Item Cart Layout

---
+++ app/src/main/java/com/binar/binarfoodapp/presentation/cart/CartItemListViewHolder.kt
@@ -0,0 +1,10 @@
+package com.binar.binarfoodapp.presentation.cart
+
+import com.binar.binarfoodapp.databinding.ItemCartBinding
+
+
+class CartItemListViewHolder(
+    private val binding: ItemCartBinding
+){
+
+}
