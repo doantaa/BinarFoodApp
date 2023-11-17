e5c2ead78f6b18ba5cb4b49aad776e554e4e1b43
doantaa <doantaaloycius@gmail.com>
2023-11-17 17:59:29 +0700

    Finish Unit Testing

--- app/src/test/java/com/binar/binarfoodapp/data/network/firebase/auth/FirebaseAuthDataSourceImplTest.kt
+++ app/src/test/java/com/binar/binarfoodapp/data/network/firebase/auth/FirebaseAuthDataSourceImplTest.kt
@@ -3,10 +3,13 @@ package com.binar.binarfoodapp.data.network.firebase.auth
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
+import io.mockk.mockkConstructor
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
@@ -54,6 +57,25 @@ class FirebaseAuthDataSourceImplTest {
         every { firebaseAuth.signInWithEmailAndPassword(any(), any()) } returns mockTaskAuthResult()
         runTest {
             val result = dataSource.doLogin("email", "password")
+            assertEquals(result, true)
+        }
+    }
+
+    @Test
+    fun `test register`() {
+        runTest {
+            mockkConstructor(UserProfileChangeRequest.Builder::class)
+            every { anyConstructed<UserProfileChangeRequest.Builder>().build() } returns mockk()
+
+            val mockAuthResult = mockTaskAuthResult()
+            every { firebaseAuth.createUserWithEmailAndPassword(any(), any()) } returns mockAuthResult
+
+            val mockUser = mockk<FirebaseUser>(relaxed = true)
+            every { mockAuthResult.result.user } returns mockUser
+
+            every { mockUser.updateProfile(any()) } returns mockTaskVoid()
+
+            val result = dataSource.doRegister("name", "email", "password")
             assertEquals(result, true)
         }
     }
